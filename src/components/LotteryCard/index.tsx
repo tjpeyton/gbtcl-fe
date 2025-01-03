35df2917cdaa32657d7484aadde95fe0e4afaf93
tjpeyton <tim.peyton@protonmail.com>
2025-01-03 08:44:18 -0500

    stylized lottery card

--- src/components/LotteryCard/index.tsx
+++ src/components/LotteryCard/index.tsx
@@ -1,14 +1,12 @@
 'use client'
 
 import { useState } from "react"
-import { Plus } from "lucide-react"
+import { InfoIcon } from "lucide-react"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
 import { StatusCircle } from "@/components/StatusCircle"
-import { FormDialog } from "@/components/dialog/FormDialog"
-import { PurchaseTicketForm } from "@/components/forms/PurchaseTicketForm"
 import { PurchaseTicketFormData } from "@/components/forms/PurchaseTicketForm/types"
+import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from "@radix-ui/react-tooltip"
 
 import { CHAIN_ID_TO_NETWORK, formatUnixTimestampFromSeconds } from "@/lib/utils"
 import { LotteryDocument } from "@/lib/types/lottery"
@@ -37,18 +35,35 @@ export const LotteryCard = ({ lottery, isBuyingTickets, onBuyTickets }: LotteryC
             <span className="text-sm text-gray-500">
               {CHAIN_ID_TO_NETWORK[lottery.contract.chainId]}
             </span>
-           
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <InfoIcon />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Created At: {formatUnixTimestampFromSeconds(lottery.createdAt)}</p>
+                  <p>Tickets Sold: {lottery.tickets.length}/{lottery.maxTickets}</p>
+                </TooltipContent>
+              </Tooltip>  
+            </TooltipProvider>
           </div>
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-row gap-24">
-          <div className="flex flex-col gap-2">
-            <span>Created At: {formatUnixTimestampFromSeconds(lottery.createdAt)}</span>
-            <span>Ticket Sales End: {formatUnixTimestampFromSeconds(lottery.expiration)}</span>
+        <div className="flex flex-row gap-2">
+          <div className="flex flex-col gap-1 border-solid border-2 border-black rounded-md p-3 w-1/2">
+            <span>Total pool:</span>
+            <span>{lottery.tickets.length * lottery.ticketPrice} wei</span>
           </div>
-          <div className="flex flex-col gap-2">
-            <span>Tickets Sold: {lottery.tickets.length}/{lottery.maxTickets}</span>
+          <div className="flex flex-col gap-1 border-solid border-2 border-black rounded-md p-3 w-1/2">
+            <span>Tickets left:</span>  
+            <span>{lottery.maxTickets - lottery.tickets.length}</span>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col gap-1 p-3 w-100">
+            <span>Next Draw</span>
+            
           </div>
         </div>
       </CardContent>
